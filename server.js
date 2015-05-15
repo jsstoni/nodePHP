10fd8ca8570450058bae04ab3b41f889d334f429
Jesus Perez <jsstoniha@gmail.com>
2015-05-14 21:40:20 -0430

    Servidor socket.io 0.9.16

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
 var socket = require( 'socket.io' );
-var http = require('http');
-var server = http.createServer(function (req, res) {
-	console.log('ServerStart');
-});
-
-var io = socket.listen(server);
+var io = socket.listen(6063);
 
 io.sockets.on('connection', function(socket) {
 	socket.on('evento', function( data ){
 		console.log(data.nombre);
 	});
 });
-
-server.listen(6063);
